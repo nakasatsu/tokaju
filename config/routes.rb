320e4001e9dc4a523a564efd56b46993e2b80a8d
EC2 Default User <ec2-user@ip-172-31-4-181.ap-northeast-1.compute.internal>
2023-02-17 09:58:52 +0000

    コントローラ、ルーティングの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,36 @@
 Rails.application.routes.draw do
+  # 管理者用
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
   
+  namespace :admin do
+    get '/' => 'homes#top'
+    get 'users/unsubscribe' => 'users#unsubscribe'
+    resources :users, only: [:index, :show, :destroy]
+    resources :posts, only: [:index, :show, :destroy]
+  end
+  
+  # 会員用
   devise_for :users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
+  
+  scope module: :public do
+    root to: 'homes#top'
+    
+    resources :posts, only: [:new, :create, :show, :destroy] do
+      resources :post_comments, only: [:create, :destroy]
+      resource :favorites, only: [:create, :destroy]
+    end
+    
+    resources :users, only: [:show, :edit, :update] do
+      member do
+        get :favorites
+      end
+    end
+    
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
