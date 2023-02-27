575c8a5ebb686d764148dd8bb348161a6de6af1a
EC2 Default User <ec2-user@ip-172-31-4-181.ap-northeast-1.compute.internal>
2023-02-27 03:40:50 +0000

    タグ検索機能を実装中

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   # 管理者用
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
@@ -34,6 +35,8 @@ Rails.application.routes.draw do
         get :favorites
       end
     end
+    
+    get '/search' => 'searches#search'
     
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
