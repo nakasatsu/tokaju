13a1f49e665170ee7ccb4f7480d4273ea2275570
EC2 Default User <ec2-user@ip-172-31-4-181.ap-northeast-1.compute.internal>
2023-02-27 07:20:16 +0000

    タグ検索機能実装

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,4 +1,10 @@
 class Public::SearchesController < ApplicationController
   def search
+    @model = params[:model]
+    @content = params[:content]
+    @method = params[:method]
+    if @model == 'tag'
+      @records = Tag.search_posts_for(@content, @method)
+    end
   end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,13 @@
 class Tag < ApplicationRecord
   has_many :post_tags, dependent: :destroy
+  has_many :posts, through: :post_tags
+  
+  def self.search_posts_for(content, method)
+    if method == 'perfect'
+      tags = Tag.where(tag_name: content)
+    end
+    
+    return tags.inject(init = []) {|result, tag| result + tag.posts}
+  end
+  
 end
