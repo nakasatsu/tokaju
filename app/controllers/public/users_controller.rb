c0b37faabb421ac50b96382f9643364157d9f0bd
EC2 Default User <ec2-user@ip-172-31-4-181.ap-northeast-1.compute.internal>
2023-03-13 06:46:58 +0000

    レイアウト調整、お気に入り投稿一覧にページネーション実装

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -6,7 +6,8 @@ class Public::UsersController < ApplicationController
   def favorites
     @user = User.find(params[:id])
     favorites = Favorite.where(user_id: @user.id).pluck(:post_id)
-    @posts = Post.find(favorites)
+    @posts = Post.where(id: favorites).page(params[:page])
+    # @posts = Post.find(favorites)
   end
   
   def show
