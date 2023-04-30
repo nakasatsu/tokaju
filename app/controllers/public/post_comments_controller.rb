eab972968644aa9425a3428e69e9419e6a5a511a
EC2 Default User <ec2-user@ip-172-31-4-181.ap-northeast-1.compute.internal>
2023-04-30 06:58:29 +0000

    コメント機能の非同期通信化

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -3,21 +3,29 @@ class Public::PostCommentsController < ApplicationController
   
   def create
     post = Post.find(params[:post_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.post_id = post.id
-    if comment.save
-      redirect_to post_path(post.id)
-    else
+    @comment = current_user.post_comments.new(post_comment_params)
+    @comment.post_id = post.id
+    unless @comment.save
       flash[:notice] = "コメントを入力してください"
       @post = Post.find(params[:post_id])
       @post_comment = PostComment.new
       render template: "public/posts/show"
     end
+    # if comment.save
+    #   redirect_to post_path(post.id)
+    # else
+    #   flash[:notice] = "コメントを入力してください"
+    #   @post = Post.find(params[:post_id])
+    #   @post_comment = PostComment.new
+    #   render template: "public/posts/show"
+    # end
   end
   
   def destroy
-    PostComment.find(params[:id]).destroy
-    redirect_to post_path(params[:post_id])
+    @comment = PostComment.find(params[:id])
+    @comment.destroy
+    # PostComment.find(params[:id]).destroy
+    # redirect_to post_path(params[:post_id])
   end
   
   private
